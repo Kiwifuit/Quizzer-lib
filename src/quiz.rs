b2de773365d06d3296bbfc53fa7f6f8486941d20
Kiwifuit <mahkiwi123@gmail.com>
2023-02-07 00:27:10 +0800

    Added `from` Impls for quiz items

--- src/quiz.rs
+++ src/quiz.rs
@@ -19,3 +19,40 @@ pub enum QuizItemType {
     FillInTheBlank,
     TrueOrFalse,
 }
+
+impl From<Vec<QuizItem>> for Quiz {
+    fn from(value: Vec<QuizItem>) -> Self {
+        Self {
+            length: value.len() as u8,
+            items: value,
+        }
+    }
+}
+
+impl From<(u8, String, String)> for QuizItem {
+    fn from(value: (u8, String, String)) -> Self {
+        let (q_type, question, answer) = value;
+
+        Self {
+            item_type: QuizItemType::try_from(q_type).unwrap(),
+            question,
+            answer,
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct InvalidNumberError(u8);
+
+impl TryFrom<u8> for QuizItemType {
+    type Error = InvalidNumberError;
+
+    fn try_from(value: u8) -> Result<Self, Self::Error> {
+        match value {
+            1 => Ok(Self::MultipleChoice),
+            2 => Ok(Self::FillInTheBlank),
+            3 => Ok(Self::TrueOrFalse),
+            other => Err(InvalidNumberError(other)),
+        }
+    }
+}
