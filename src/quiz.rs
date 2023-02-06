0da4c5b34ab3fa9bc639f759761e9cf3ca1e636a
Kiwifuit <mahkiwi123@gmail.com>
2023-02-07 00:07:33 +0800

    Added basic structure

--- src/quiz.rs
+++ src/quiz.rs
@@ -1,1 +1,16 @@
+pub struct Quiz {
+    length: u8,
+    items: Vec<QuizItem>,
+}
 
+pub struct QuizItem {
+    item_type: QuizItemType,
+    question: String,
+    answer: String,
+}
+
+pub enum QuizItemType {
+    MultipleChoice,
+    FillInTheBlank,
+    TrueOrFalse,
+}
