41a8bba8dd3144378308c4286c5f9ce59395f79d
Kiwifuit <mahkiwi123@gmail.com>
2023-02-07 00:19:38 +0800

    Implemented de/serialize functions

--- src/data.rs
+++ src/data.rs
@@ -1,1 +1,22 @@
+use std::io::{Read, Write};
+
+use crate::quiz::Quiz;
 use bincode::{deserialize_from, serialize_into};
+use serde::Serialize;
+
+type DataError = bincode::Error;
+
+pub fn load<R>(file: &mut R) -> Result<Quiz, DataError>
+where
+    R: Read,
+{
+    deserialize_from(file)
+}
+
+pub fn dump<W, D>(file: &mut W, data: &D) -> Result<(), DataError>
+where
+    W: Write,
+    D: Serialize,
+{
+    serialize_into(file, data)
+}
