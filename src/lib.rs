985e405dc02087c476f109b0759637edf06c16fd
Kiwifuit <mahkiwi123@gmail.com>
2023-02-07 09:05:17 +0800

    Improved test suite

--- src/lib.rs
+++ src/lib.rs
@@ -4,33 +4,10 @@ pub mod quiz;
 #[cfg(test)]
 mod test {
     use super::*;
-    use std::io::Cursor;
-
-    #[allow(unused_must_use)]
-    #[test]
-    fn quiz_item_instantiates() {
-        let raw_values = vec![
-            (1u8, "Do you love her?", "yes"),
-            (1u8, "Amoogns cronut", "no"),
-            (1u8, "Weeeeeeeeeeee", "yes"),
-            (1u8, "Fourth question?", "yes"),
-            (1u8, "Is the next question of the same type?", "no"),
-            (2u8, "Who is the bestest doggo in the world?", "Rosie"),
-            (2u8, "Who is the smelliest doggo in the world?", "Tasha"),
-            (3u8, "What is life?", "Damn bro idk"),
-            (
-                3u8,
-                "What is this hit game called Amongus?",
-                "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA",
-            ),
-        ];
-
-        quiz::Quiz::from(raw_values);
-    }
+    use std::fs::OpenOptions;
 
     #[test]
     fn serializes() {
-        let mut file = Cursor::new(Vec::new());
         let raw_values = vec![
             (1u8, "Do you love her?", "yes"),
             (1u8, "Amoogns cronut", "no"),
@@ -47,8 +24,11 @@ mod test {
             ),
         ];
 
-        let quiz = quiz::Quiz::from(raw_values);
+        let file = OpenOptions::new().write(true).create(true).open("dump.bin");
+        assert!(file.is_ok());
+        let mut file = file.unwrap();
 
+        let quiz = quiz::Quiz::from(raw_values);
         let res = data::dump(&mut file, &quiz);
 
         assert!(res.is_ok());
@@ -56,7 +36,6 @@ mod test {
 
     #[test]
     fn deserializes() {
-        let mut file = Cursor::new(Vec::new());
         let raw_values = vec![
             (1u8, "Do you love her?", "yes"),
             (1u8, "Amoogns cronut", "no"),
@@ -73,10 +52,11 @@ mod test {
             ),
         ];
 
-        let quiz = quiz::Quiz::from(raw_values);
+        let file = OpenOptions::new().read(true).open("dump.bin");
+        assert!(file.is_ok());
+        let mut file = file.unwrap();
 
-        let res = data::dump(&mut file, &quiz);
-        assert!(res.is_ok());
+        let quiz = quiz::Quiz::from(raw_values);
 
         let res = data::load(&mut file);
         if let Err(e) = &res {
